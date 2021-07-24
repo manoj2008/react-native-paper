333de6ac4e2d82e436dd55790e3ca6bb572c437d
Bruno Castro <bruno.castro@codeminer42.com>
2021-07-24 13:24:33 -0300

    fix: add missing padding to CardTitle

--- src/components/Card/CardTitle.tsx
+++ src/components/Card/CardTitle.tsx
@@ -158,7 +158,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    paddingLeft: 16,
+    paddingHorizontal: 16,
   },
 
   left: {
